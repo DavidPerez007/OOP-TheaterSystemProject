dfe8b2f28fc0a08ac635cb10262437ebe8d71380
David Pérez <97565939+DavidPerez007@users.noreply.github.com>
2022-11-12 11:46:38 -0600

    Adding user CRUD files

---
+++ Usuarios/src/Modelo/Administrador.java
@@ -0,0 +1,29 @@
+
+package Modelo;
+
+public class Administrador extends Usuario{
+    
+    public Administrador(String nombre, String apellido, String curp, String nombreUsuario, String password){
+        super( nombre, apellido, curp, nombreUsuario, password);
+    }
+    
+    public void agregarAdministrador(){
+        
+    }
+    
+    public void agregarEmpleado(){
+        
+    }
+    
+    public void modificarCuenta(){
+        
+    }
+    
+    public void eliminarCuenta(){
+        
+    }
+    
+    public void buscarUsuario(){
+        
+    }
+}
---
+++ Usuarios/src/Modelo/Empleado.java
@@ -0,0 +1,9 @@
+package Modelo;
+
+public class Empleado extends Usuario {
+    
+    public Empleado(String nombre, String apellido, String curp, String nombreUsuario, String password){
+        super(nombre, apellido, curp, nombreUsuario, password);
+    }
+    
+}
