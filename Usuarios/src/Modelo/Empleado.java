21bbf9c61fcabc9cee60d0a7fb598d56fd0cc255
David Pérez <97565939+DavidPerez007@users.noreply.github.com>
2022-11-30 20:45:18 -0600

    Delete Usuarios directory

--- Usuarios/src/Modelo/Empleado.java
+++
@@ -1,9 +0,0 @@
-package Modelo;
-
-public class Empleado extends Usuario {
-    
-    public Empleado(String nombre, String apellido, String curp, String nombreUsuario, String password){
-        super(nombre, apellido, curp, nombreUsuario, password);
-    }
-    
-}
